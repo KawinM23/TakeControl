3d9b9c8fe03f5f745d3e27687431b5a507620830
KawinM23 <kawin.m235@gmail.com>
2024-02-14 20:12:15 +0700

    Add basic movement

---
+++ Assets/Scripts/Capabilities/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Capabilities
+{
+    [RequireComponent(typeof(Controller))]
+    public class Move : MonoBehaviour
+    {
+        [SerializeField, Range(0f, 100f)] private float _maxSpeed = 4f;
+
+        private Controller _controller;
+        private Vector2 _direction, _desiredVelocity, _velocity;
+        private Rigidbody2D _body;
+        private Ground _ground;
+
+        private void Awake()
+        {
+            _body = GetComponent<Rigidbody2D>();
+            _ground = GetComponent<Ground>();
+            _controller = GetComponent<Controller>();
+        }
+
+        private void Update()
+        {
+            _direction.x = _controller.input.RetrieveMoveInput();
+            _desiredVelocity = new Vector2(_direction.x, 0f) * Mathf.Max(_maxSpeed - _ground.Friction, 0f);
+        }
+
+        private void FixedUpdate()
+        {
+            _velocity = _body.velocity;
+
+            _velocity.x = _desiredVelocity.x;
+
+            _body.velocity = _velocity;
+        }
+    }
+}
