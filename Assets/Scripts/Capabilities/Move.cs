fb1808ea8e0c4aa8ab4d239bc28a5d57cce45331
KawinM23 <kawin.m235@gmail.com>
2024-02-20 21:27:10 +0700

    Add direction to dash

--- Assets/Scripts/Capabilities/Move.cs
+++ Assets/Scripts/Capabilities/Move.cs
@@ -15,16 +15,16 @@ namespace Assets.Scripts.Capabilities
         private SpriteRenderer _spriteRenderer;
 
         [SerializeField, Range(0f, 100f)] private float _maxSpeed = 4.2f;
-      
+
         [SerializeField, Range(0f, 100f)] private float dashingPower = 20f;
         [SerializeField, Range(0f, 10f)] private float dashingCooldown = 1f;
-        [SerializeField, Range(0f, 1f)]  private float dashingTime = 0.2f;
+        [SerializeField, Range(0f, 1f)] private float dashingTime = 0.2f;
         private bool canDash = true;
         private bool isDashing;
-        private bool isFacingRight;
+        public bool isFacingRight;
         private bool followMovement; //facing follow the movement
-        
-      
+
+
 
         private void Awake()
         {
@@ -33,6 +33,7 @@ namespace Assets.Scripts.Capabilities
             _controller = GetComponent<Controller>();
             _spriteRenderer = transform.Find("PlayerSprite").GetComponent<SpriteRenderer>();
             followMovement = true;
+
         }
 
         private void Update()
@@ -41,69 +42,58 @@ namespace Assets.Scripts.Capabilities
             _desiredVelocity = new Vector2(_direction.x, 0f) * Mathf.Max(_maxSpeed - _ground.Friction, 0f);
             if (followMovement)
             {
-                if(_desiredVelocity.x > 0)
-                {
-                    isFacingRight = true;
-                    
-                }
-                else if( _desiredVelocity.x < 0)
-                {
-                    isFacingRight = false;
-                    
-                }
+                isFacingRight = _desiredVelocity.x > 0 ? true : (_desiredVelocity.x < 0 ? false : isFacingRight);
             }
-            _spriteRenderer.flipX = !isFacingRight;
+            if (_spriteRenderer) { _spriteRenderer.flipX = !isFacingRight; }
 
             if (_controller.input.RetrieveDashInput() && canDash)
             {
-                StartCoroutine(Dash());
+                StartCoroutine(Dash(_direction.x, isFacingRight));
             }
-            
+
         }
-
-
 
         private void FixedUpdate()
         {
-
             if (isDashing)
             {
-                return;
+                _body.gravityScale = 0f;
             }
-
-            _body.velocity = new Vector2( dashingPower, _body.velocity.y);
-            _velocity = _body.velocity;
-
-            _velocity.x = _desiredVelocity.x;
-
+            else
+            {
+                _velocity = _body.velocity;
+                _velocity.x = _desiredVelocity.x;
+            }
             _body.velocity = _velocity;
-            
+
         }
 
-        private IEnumerator Dash()
+        private IEnumerator Dash(float x, bool isFacingRight)
         {
             canDash = false;
             isDashing = true;
             float originalGravity = _body.gravityScale;
-            _body.gravityScale = 0f;
-            _body.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
-           
+            float direction;
+            if (x > 0)
+            {
+                direction = 1f;
+            }
+            else if (x < 0)
+            {
+                direction = -1f;
+            }
+            else
+            {
+                direction = isFacingRight ? 1f : -1f;
+            }
+            _velocity = new Vector2(dashingPower * direction, 0f);
+
             yield return new WaitForSeconds(dashingTime);
-            
-            _body.gravityScale = originalGravity;
             isDashing = false;
+            _body.gravityScale = originalGravity;
+
             yield return new WaitForSeconds(dashingCooldown);
             canDash = true;
-        }
-
-        public bool IsFacingRight()
-        {
-            return isFacingRight;
-        }
-
-        public void SetIsFacingRight(bool isFacingRight)
-        {
-            this.isFacingRight = isFacingRight;
         }
 
         public void SetFollowMovement(bool followMovement)
