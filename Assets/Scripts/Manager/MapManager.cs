9a1ae8f6166bb360ada25eba7055ed4981c15965
KawinM23 <kawin.m235@gmail.com>
2024-02-20 19:41:56 +0700

    Add MapManager and MapTransition

---
+++ Assets/Scripts/Manager/MapManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapManager : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+
+    private void Start()
+    {
+        player = Player.FindActivePlayer();
+        Debug.Log(player);
+        DontDestroyOnLoad(player);
+    }
+
+    public IEnumerator ChangeScene(string fromScene,string toScene)
+    {
+        yield return SceneManager.LoadSceneAsync(toScene, LoadSceneMode.Additive);
+        SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByName(toScene));
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(toScene));
+        player.transform.position = MapTransition.FindDestinationPosition(fromScene, toScene);
+        player.GetComponent<TrailRenderer>().Clear();
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName(fromScene));
+
+        yield return null;
+    }
+}
