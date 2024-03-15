be2083b4aa12f790ae396bab7e90ccd7554e99a4
Thanadol Chitthamlerd <thanadolps@gmail.com>
2024-03-15 18:03:12 +0700

    feat(camera): move necessity to "setup" prefab and add confine camera boundary.

--- Assets/Scripts/Manager/CameraManager.cs
+++ Assets/Scripts/Manager/CameraManager.cs
@@ -2,15 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using Assets.Scripts.Capabilities;
 using UnityEngine.SceneManagement;
-using TMPro;
 
 public class CameraManager : MonoBehaviour
 {
+    public static CameraManager Instance { get; private set; }
+
     [SerializeField] private CinemachineVirtualCamera _camera;
 
-    // Start is called before the first frame update
+    void Awake()
+    {
+        // Singleton pattern
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
     void Start()
     {
         if (_camera == null)
@@ -18,22 +30,28 @@ public class CameraManager : MonoBehaviour
             _camera = FindObjectOfType<CinemachineVirtualCamera>();
         }
         Debug.Assert(_camera != null, "Camera is not set in CameraManager");
-
         _camera.Follow = PlayerManager.Instance.playerGameObject.transform;
+
         PlayerManager.OnPlayerChanged += (player) => _camera.Follow = player.transform;
 
+        SceneManager.sceneLoaded += (_, _) =>
+        {
+            _camera = FindObjectOfType<CinemachineVirtualCamera>();
+            _camera.Follow = PlayerManager.Instance.playerGameObject.transform;
 
-        // Remove non-cinemachine cameras
-        // SceneManager.sceneLoaded += (_, _) =>
-        // {
-        //     foreach (var c in FindObjectsByType<Camera>(FindObjectsSortMode.None))
-        //     {
-        //         if (!c.TryGetComponent<CinemachineBrain>(out var _))
-        //         {
-        //             Debug.Log("Destroying camera");
-        //             Destroy(c.gameObject);
-        //         }
-        //     }
-        // };
+            // RemoveNonCinemachineCameras();
+        };
+    }
+
+    private void RemoveNonCinemachineCameras()
+    {
+        foreach (var c in FindObjectsByType<Camera>(FindObjectsSortMode.None))
+        {
+            if (!c.TryGetComponent<CinemachineBrain>(out var _))
+            {
+                Debug.Log("Destroying camera");
+                Destroy(c.gameObject);
+            }
+        }
     }
 }
