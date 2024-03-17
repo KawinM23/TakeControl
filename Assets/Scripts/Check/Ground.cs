62c342a402ccefc83e1a61e14a090296a8114a77
KawinM23 <kawin.m235@gmail.com>
2024-03-17 16:11:20 +0700

    Fix colliding with no rigidbody

--- Assets/Scripts/Check/Ground.cs
+++ Assets/Scripts/Check/Ground.cs
@@ -39,7 +39,7 @@ public class Ground: MonoBehaviour
 
     private void RetrieveFriction(Collision2D collision)
     {
-        _material = collision.rigidbody.sharedMaterial;
+        collision.transform.TryGetComponent(out _material);
 
         Friction = 0;
 
