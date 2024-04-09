0db85f787ed719e8cc217199a16a8c77b38afecf
TuchthamSung086 <touch0869@gmail.com>
2024-04-09 23:54:05 +0700

    feat: arena scene

--- Assets/Scripts/AIController/SpawnerController.cs
+++ Assets/Scripts/AIController/SpawnerController.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using UnityEngine;
 
+// SpawnerController spawns one type of GameObject in waves
 public class SpawnerController : MonoBehaviour
 {
-    [SerializeField] private GameObject _bomberPrefab; // The prefab to spawn
-    [SerializeField] private float _spawnInterval = 1f; // The interval between spawns
-    [SerializeField] private int _waveCount = 3; // The number of waves to spawn
+    [SerializeField] private float _delayedStart = 0f; // The delay before the first wave
+    [SerializeField] private GameObject _prefab; // The prefab to spawn
+    [SerializeField] private int _spawnCountMax = 20;
+    [SerializeField] private int _spawnCountMin = 1;
+    [SerializeField] private float _spawnIntervalMax = 1f;
+    [SerializeField] private float _spawnIntervalMin = 0f;
+
+    [SerializeField] private int _waveCount = 12; // The number of waves to spawn
     [SerializeField] private float _waveInterval = 5f; // The interval between waves
 
     [SerializeField] private int _currentWave = 0; // The current wave number
@@ -18,27 +24,41 @@ public class SpawnerController : MonoBehaviour
 
     private IEnumerator SpawnWaves()
     {
+        if (_currentWave == 0)
+        {
+            yield return new WaitForSeconds(_delayedStart);
+        }
+
         while (_currentWave < _waveCount)
         {
             // Spawn a wave
-            SpawnWave();
+            yield return SpawnWave();
 
             // Wait for the wave interval
             yield return new WaitForSeconds(_waveInterval);
         }
     }
 
-    private void SpawnWave()
+    private IEnumerator SpawnWave()
     {
         // Spawn enemies or objects in the wave
         // You can customize the spawn positions, rotations, and other configurations here
 
+        // Randomize spawn count in range
+        int spawnCount = Random.Range(_spawnCountMin, _spawnCountMax);
+
         // Example: Spawn 5 enemies in a line
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < spawnCount; i++)
         {
             Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + 0.5f, 0f);
             Quaternion spawnRotation = Quaternion.identity;
-            Instantiate(_bomberPrefab, spawnPosition, spawnRotation);
+            Instantiate(_prefab, spawnPosition, spawnRotation);
+
+            // Randomize spawn interval
+            float spawnInterval = Random.Range(_spawnIntervalMin, _spawnIntervalMax);
+
+            // Wait for the spawn interval
+            yield return new WaitForSeconds(spawnInterval);
         }
 
         // Increase the wave number
