520cfae295ad1867043395515e249e1a6bad96ef
KawinM23 <kawin.m235@gmail.com>
2024-04-09 11:33:21 +0700

    Add die particle

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -24,6 +24,9 @@ namespace Assets.Scripts.Combat
         [SerializeField] private LayerMask _iFramableLayer;
         [SerializeField] private PlayerManager _playerManager;
 
+        [Header("Effect")]
+        [SerializeField] ParticleSystem _dieParticle;
+
 
         private SpriteRenderer _spriteRenderer;
         private readonly Collider2D _collider;
@@ -35,6 +38,9 @@ namespace Assets.Scripts.Combat
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _spriteRenderer = transform.Find("Sprite").GetComponent<SpriteRenderer>();
+            _originalColor = _spriteRenderer.color;
+            _playerManager = PlayerManager.Instance;
         }
 
 
@@ -44,8 +50,6 @@ namespace Assets.Scripts.Combat
             _currentHealth = _maxHealth;
             _iFrame = false;
             _iFrameCounter = 0f;
-            _spriteRenderer = transform.Find("Sprite").GetComponent<SpriteRenderer>();
-            _playerManager = PlayerManager.Instance;
         }
 
         // Update is called once per frame
@@ -100,7 +104,6 @@ namespace Assets.Scripts.Combat
 
         IEnumerator Flash(Color targetColor)
         {
-            _originalColor = _spriteRenderer.color;
             _spriteRenderer.color = targetColor;
             yield return new WaitForSeconds(0.1f);
             _spriteRenderer.color = _originalColor;
@@ -120,6 +123,11 @@ namespace Assets.Scripts.Combat
             {
                 _playerManager.Die();
             }
+            /*SoundManager.Instance;*/
+            var main = _dieParticle.main;
+            main.startColor = _originalColor;
+            GameObject go = Instantiate(_dieParticle.gameObject, gameObject.transform.position, Quaternion.identity);
+            Destroy(go, 2f);
             Destroy(gameObject);
         }
 
