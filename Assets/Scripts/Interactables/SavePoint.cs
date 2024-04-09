68d65aace8a29ee79f0a73295e5a8426ae8d20bd
KawinM23 <kawin.m235@gmail.com>
2024-04-09 13:59:26 +0700

    Edit SavePoint

--- Assets/Scripts/Interactables/SavePoint.cs
+++ Assets/Scripts/Interactables/SavePoint.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SavePoint : Interactable
 {
+    private string _sceneName;
+
+    private void Awake()
+    {
+        _sceneName = gameObject.scene.name;
+    }
+
     public override void Interact()
     {
-        Debug.Log("SAVE POINT");
+        Debug.Log("SAVE POINT " + _sceneName);
     }
 }
