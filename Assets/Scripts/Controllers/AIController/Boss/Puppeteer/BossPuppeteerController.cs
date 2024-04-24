31dbc9c50a6b61f72ccf96a3d843a9b886672c22
KawinM23 <kawin.m235@gmail.com>
2024-04-24 23:54:01 +0700

    Fix bugs

--- Assets/Scripts/Controllers/AIController/Boss/Puppeteer/BossPuppeteerController.cs
+++ Assets/Scripts/Controllers/AIController/Boss/Puppeteer/BossPuppeteerController.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using Assets.Scripts.Combat;
-using Assets.Scripts.Effect;
+using System.Collections;
 using UnityEngine;
 
 #nullable enable annotations
